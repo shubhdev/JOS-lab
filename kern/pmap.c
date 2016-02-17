eea24ba134c07d2f3e82dd059118f1eb5aa1ff28
Shubham Rawat <shubhamrawat1100@gmail.com>
2016-02-17 19:47:11 +0530

    update final submission

--- kern/pmap.c
+++ kern/pmap.c
@@ -187,7 +187,7 @@ mem_init(void)
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
-	boot_map_region(kern_pgdir,UPAGES,npages*sizeof(struct PageInfo),PADDR(pages),PTE_P|PTE_U);
+	boot_map_region(kern_pgdir,UPAGES,PTSIZE,PADDR(pages),PTE_P|PTE_U);
 	//boot_map_region(kern_pgdir,(uintptr_t)pages,npages*sizeof(struct PageInfo),PADDR(pages),PTE_P|PTE_W);
 	///////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
