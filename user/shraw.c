7285ac6a2f6bca0d2ec827af557f9455ce35ab2b
Anupam <you@example.com>
2016-04-14 04:47:11 -0700

    parsecmd added (checked)

--- user/shraw.c
+++ user/shraw.c
@@ -17,6 +17,51 @@
 //   char *argv[MAXARGS];
 //   char *eargv[MAXARGS];
 // };
+
+char cmd_line[1024];
+
+int isWhiteSpace(char a)
+{
+	
+	if(a==' ' || a=='\t' || a=='\n' || a=='\v' || a=='\r') return 1;
+	return 0;
+}
+
+int parsecmd(char* cmd_inp)
+{
+	memset(cmd_line , 0 , 1024);
+	int j=0;
+	while(isWhiteSpace(cmd_inp[j]))
+	{
+		j++;
+	}
+	int cmd_len = strlen(cmd_inp),i=0,sz=0,blank_space =0;
+	//empty input
+	if(cmd_len == 0) return -1;
+	int args = 1;
+	for(i=j;i<cmd_len;i++)
+	{
+		if(isWhiteSpace(cmd_inp[i]))
+		{
+			blank_space=1;
+			continue;
+		}
+
+		if(blank_space) 
+		{
+			cmd_line[sz++] = '\0';
+			args +=1;
+		}
+		cmd_line[sz++] = cmd_inp[i];
+		blank_space=0;
+
+	}
+
+	return args;
+}
+
+
+
 void runcmd(int util_id,int arg){
 	
 	if(sys_exec(util_id,arg) < 0)
